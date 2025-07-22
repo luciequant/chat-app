dc3b753954b088bab4e5969a434b2cfcdadcf72b
luciequant <156855912+luciequant@users.noreply.github.com>
2025-07-22 12:51:24 +0200

    Realtime message

--- src/components/ChatMessages.tsx
+++ src/components/ChatMessages.tsx
@@ -1,6 +1,7 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { useChatStore } from "../store/chatStore";
 import { supabase } from "../supabaseClient";
+import { useEffect } from "react";
 
 interface Message {
   id: number;
@@ -24,6 +25,7 @@ async function fetchMessages(roomId: number): Promise<Message[]> {
 
 function ChatMessages() {
   const { currentRoom, user } = useChatStore();
+  const queryClient = useQueryClient();
   const {
     data: messages,
     error,
@@ -37,16 +39,44 @@ function ChatMessages() {
     enabled: currentRoom?.id !== null,
   });
 
-  if (isLoading) return <p className="loader-text">Loading messages...</p>;
+  useEffect(() => {
+    if (!currentRoom?.id) return;
 
+    const channel = supabase.channel("messages-channel");
+
+    channel
+      .on(
+        "postgres_changes",
+        { event: "INSERT", schema: "public", table: "messages" },
+        (payload) => {
+          const newMessage = payload.new as Message;
+          if (newMessage.room_id === currentRoom.id) {
+            queryClient.setQueryData<Message[]>(
+              ["messages", currentRoom?.id],
+              (oldMessages) =>
+                oldMessages ? [...oldMessages, newMessage] : [newMessage]
+            );
+          }
+        }
+      )
+      .subscribe((status) => {
+        console.log("Sub status: ", status);
+      });
+
+    return () => {
+      supabase.removeChannel(channel);
+    };
+  }, [currentRoom?.id]);
+
+  if (isLoading) return <p className="loader-text"> Loading messages...</p>;
   if (error)
     return (
-      <p className="loader-text">Error loading messages: {error.message}</p>
+      <p className="loader-text"> Error loading messages: {error.message} </p>
     );
-  console.log(messages);
 
   return (
     <>
+      {" "}
       {messages?.map((msg: Message, key) => {
         const isOwnMessage = msg.user_id === user?.id;
         const itemClass = isOwnMessage ? "right" : "left";
@@ -55,15 +85,14 @@ function ChatMessages() {
         const hour = date.getHours().toString().padStart(2, "0");
         const minute = date.getMinutes().toString().padStart(2, "0");
         const formattedTime = `${hour}:${minute}`;
-
         return (
           <div
             key={key}
             className={`conv-message-item conv-message-item--${itemClass}`}
           >
-            <div className="conv-message-value">{msg.content}</div>
-            <div className="conv-message-details"> {formattedTime}</div>
-            <div className="conv-message-details">{msg.email}</div>
+            <div className="conv-message-value"> {msg.content} </div>
+            <div className="conv-message-details"> {formattedTime} </div>
+            <div className="conv-message-details"> {msg.email} </div>
           </div>
         );
       })}
