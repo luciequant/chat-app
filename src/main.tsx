6d4111de43750d577349066f412e8af41a42efad
luciequant <156855912+luciequant@users.noreply.github.com>
2025-07-22 12:28:14 +0200

    Display Messages

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,19 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
 import { BrowserRouter as Router } from "react-router";
+
+const client = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <QueryClientProvider client={client}>
+      <Router>
+        <App />
+      </Router>
+    </QueryClientProvider>
   </StrictMode>
 );
--- src/pages/ChatRoom.tsx
+++ src/pages/ChatRoom.tsx
@@ -1,5 +1,28 @@
+import { useChatStore } from "../store/chatStore";
+import ChatMessages from "../components/ChatMessages";
+import ChatMessageForm from "../components/ChatMessageForm";
+
 function ChatRoom() {
-  return <div>Chat Room</div>;
+  const { user, currentRoom } = useChatStore();
+
+  if (!currentRoom) {
+    return <p>Please join or create a room first.</p>;
+  }
+
+  return (
+    <div className="conv">
+      <div className="conv-title">
+        {currentRoom?.name} - {user?.email}
+      </div>
+
+      <div className="conv-timeline">
+        <ChatMessages />
+      </div>
+      <div className="conv-send-message">
+        <ChatMessageForm />
+      </div>
+    </div>
+  );
 }
 
 export default ChatRoom;
